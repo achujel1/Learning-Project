1bb6794b2ac9fc528c92cdee7090760bfa7b79a4
achujel1 <achujel1@gmail.com>
2015-02-06 00:46:15 +0200

    Part 3
    4. Arguments
    
    Working with argument passing to program

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		// getting to know how many arguments are passes to program
+		System.out.println("Number of args: " + args.length);
+		
+		// printing out arguments
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
 	}
 }
